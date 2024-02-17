a28501ca093bc13899c51fb0a96ff5bdbe644603
Dzmitry Baryshau <dima.baryshev@live.com>
2024-02-17 21:42:56 +0300

    add auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
+const Home = () => (
+  <div>
+    <h1 className='h1-bold'> Welcome to Next.js 13</h1>
+    <h2 className='h2-bold'> Welcome to Next.js 13</h2>
+    <h3 className='h3-bold'> Welcome to Next.js 13</h3>
+    <UserButton afterSignOutUrl='/' />
+  </div>
+);
+
+export default Home;
